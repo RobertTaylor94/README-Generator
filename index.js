1780d78839c80c43712355873afc0ae3a1e43f32
Robert Taylor <rjt903@gmail.com>
2023-02-19 14:12:00 +0000

    Added writeFile function

--- index.js
+++ index.js
@@ -8,17 +8,23 @@ const questions = [
 
 ];
 
-
+inquirer
+    .prompt(questions).then(response => {
+        console.log(response);
+        // let fileName = generateMarkdown(response);
+        // writeToFile(fileName, fileName);
+    })
 
 // function to write README file
 function writeToFile(fileName, data) {
-    
+    fs.writeFile(`${fileName}.md`, data, function (err) {
+        err ? console.error(err) : console.log("README Saved");
+    })
 }
 
 // function to initialize program
 function init() {
 
 }
-
 // function call to initialize program
 init();
